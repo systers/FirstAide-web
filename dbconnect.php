3c0e1e1fc93ac97a195f62fafd254ae5b0804caf
AkankshaBodhankar <kmit.akankshabodhankar@gmail.com>
2016-08-15 22:50:49 +0530

    pushed work done in GSOC16

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+/*Created by Akanksha
+  Desc: Connection to mysql db for use in other files*/
+    
+    //change the credentials here according to the server used
+	$username = "root";
+	$password="";
+	$hostname="localhost";
+	$database="pcsa_web";
+	$connection = mysqli_connect($hostname,$username,$password,$database)or die("Connection failed");
+?>
