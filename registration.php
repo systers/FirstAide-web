9971395433645b8043c2a9ed222abeed0148e73d
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-03-08 12:31:01 +0530

    Fixes #64: Added confirm password

--- registration.php
+++ registration.php
@@ -39,6 +39,13 @@
            </div>
 
          </td>
+         </tr>
+        <tr>
+         <th class="text" style="vertical-align: top">Confirm Password:</th>
+         <td>
+           <input class="input-box" type="password" id="confirmpassword" name="confirmpassword" required/>
+
+         </td>
         <tr>
          <th class="text">Host Country:</th>
          <td><input class="input-box" type="text" id="host_country" name="host_country" placeholder="India" required/></td>
