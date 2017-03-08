6cf22cec3f04e52dee2a05f8bdb87fe8aab9ca26
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-03-09 00:16:53 +0530

    Fixes #62 : Added reCaptcha on signup

---
+++ includes/settings.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Date       : 9-03-2017
+ * Created by : Fatima Rafiqui
+ * 
+ * Note       : Please do not commit your credentials
+ */
+$_settings['reCaptcha'] = array();
+$_settings['reCaptcha']['client_key'] = '';
+$_settings['reCaptcha']['server_key'] = '';
+?>
