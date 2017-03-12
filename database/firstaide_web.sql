bd31a3ac175caaa78f6d654d8129ce2fb3ea6f29
shreyans29 <shreyansshrimal.ss@gmail.com>
2017-03-12 15:13:00 +0530

    Changed Database name to firstaide_web
    Solved issue #140 in order to make the database consistent with the
    latest project name.

--- database/firstaide_web.sql
+++ database/firstaide_web.sql
@@ -1,3 +1,14 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 12, 2017 at 10:38 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+CREATE DATABASE IF NOT EXISTS firstaide_web;
+USE firstaide_web;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -8,10 +19,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pcsa_web`
+-- Database: `firstaide_web`
 --
-CREATE DATABASE IF NOT EXISTS pcsa_web;
-USE pcsa_web;
 
 DELIMITER $$
 --
@@ -49,6 +58,18 @@ CREATE TABLE `comrade` (
   `comrade_email` varchar(100) CHARACTER SET latin1 DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `comrade`
+--
+
+INSERT INTO `comrade` (`comradeid`, `email`, `phonenumber`, `comrade_email`) VALUES
+(1, 'shreyansshrimal.ss@gmail.com', NULL, NULL),
+(2, 'shreyansshrimal.ss@gmail.com', NULL, NULL),
+(3, 'shreyansshrimal.ss@gmail.com', NULL, NULL),
+(4, 'shreyansshrimal.ss@gmail.com', NULL, NULL),
+(5, 'shreyansshrimal.ss@gmail.com', NULL, NULL),
+(6, 'shreyansshrimal.ss@gmail.com', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -61,6 +82,13 @@ CREATE TABLE `user` (
   `password` varchar(100) CHARACTER SET latin1 NOT NULL,
   `host_country` varchar(100) CHARACTER SET latin1 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`email`, `username`, `password`, `host_country`) VALUES
+('shreyansshrimal.ss@gmail.com', 'shreyans29', '64a168e945d96ca780639e492c07215a', 'Uganda');
 
 --
 -- Indexes for dumped tables
