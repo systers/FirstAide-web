e4d41d4e346273665782eea538cb0967f18e5424
muskanbararia <muskanbararia@gmail.com>
2017-04-03 15:39:55 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -23,6 +23,7 @@
 <html>
 <head>
     <title>FirstAide</title>
+    <link rel="shortcut icon" href="favicon.png" > //adding favicon
     <link rel="stylesheet" type="text/css" href="css files/menu-css.css"/>
 </head>
 <body>
