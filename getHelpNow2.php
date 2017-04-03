6cbd053422541e9bd1eebd865e9f5c94d32c5de7
muskanbararia <muskanbararia@gmail.com>
2017-04-03 15:38:05 +0530

    Update getHelpNow2.php

--- getHelpNow2.php
+++ getHelpNow2.php
@@ -14,6 +14,7 @@
 <html>
 <head>
   <title>FirstAide</title>
+  <link rel="shortcut icon" href="favicon.png" > //adding favicon
   <link rel="stylesheet" type="text/css" href="css files/gethelpnow-style.css"/>
 </head>
 <body>
