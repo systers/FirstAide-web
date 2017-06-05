c391aa48f38e360f099ed21de511d26fad6101aa
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-06-05 22:28:25 +0530

    Cleanup of menu.js

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -2,8 +2,8 @@ $('.sub-menu-header').click(function() {
 	var thisParent = $(this).parent();
 	var thisMenu = thisParent.find('.menu');
 	if (thisMenu.is(':hidden')) {
-		thisParent.find('.menu').slideDown( "slow");
+		thisMenu.slideDown( "slow");
 	} else {
-		thisParent.find('.menu').slideUp( "slow");
+		thisMenu.slideUp( "slow");
 	}
 });
