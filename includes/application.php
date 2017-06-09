09fe4d9ea0fb9b7513fef7433ee3fd1f814f0d75
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-06-09 23:51:55 +0530

    PSR-2 Enforcement

--- includes/application.php
+++ includes/application.php
@@ -1,13 +1,15 @@
 <?php
     $APPLICATION_DIR = $APPLICATION_DIR ?? str_replace('includes', '', dirname(__FILE__));
-    spl_autoload_register(function ($class_name) {
-        global $APPLICATION_DIR;
-        include $APPLICATION_DIR.'/modules/'.$class_name . '.php';
-    });
+    spl_autoload_register(
+        function ($class_name) {
+            global $APPLICATION_DIR;
+            include $APPLICATION_DIR.'/modules/'.$class_name . '.php';
+        }
+    );
 
-    require_once($APPLICATION_DIR.'/includes/settings.php');
-    require_once($APPLICATION_DIR.'/includes/db_connection.php');
-    require_once($APPLICATION_DIR.'/vendor/autoload.php');
+    require_once $APPLICATION_DIR.'/includes/settings.php';
+    require_once $APPLICATION_DIR.'/includes/db_connection.php';
+    require_once $APPLICATION_DIR.'/vendor/autoload.php';
 
     if (!isset($_SESSION)) {
         session_start();
