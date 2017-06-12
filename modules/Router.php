8495d50fef6dc8b16a2d2ad7c605d73b4b771919
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-06-12 22:26:09 +0530

    add pages to router

--- modules/Router.php
+++ modules/Router.php
@@ -2,8 +2,11 @@
 
 class Router
 {
+    const PAGE_REQUEST_PARAM = 'page_request';
+    const PAGE_CONTENT = 'query';
+
     const INDEX = 'index';
-    const HOME = 'home';
+    const HOME = 'home'; 
 
     const PAGE_SEEKING_STAFF_SUPPORT = 'seeking-staff-support';
     const PAGE_SERVICES_AFTER_ASSAULT = 'services-after-assault';
@@ -14,6 +17,10 @@ class Router
     const PAGE_SAFETY_PLAN_BASICS = 'safety-plan-basics';
     const PAGE_UNWANTED_ATTENTION = 'unwanted-attention';
     const PAGE_BYSTANDER_INTERVENTION = 'bystander-intervention';
+    const PAGE_COMMON_QUESTIONS = 'common-questions';
+    const PAGE_SAFETY_PLAN_WORKSHEET = 'safety-plan-worksheet';
+    const PAGE_SEXUAL_ASSAULT = 'sexual-assault';
+     const PAGE_SEXUAL_ASSAULT_MORE = 'sexual-assault-more';
 
     const COUNTRY_LIST_FILE = '/javascripts/country_list.json';
     const LOGIN_SUCCESS_URL = HOST.'?page_request='.self::HOME;
@@ -42,6 +49,30 @@ class Router
                             'title' => 'Benefits of Seeking Staff Support',
                             'card_content' => array(
                                 'twig' => 'seeking_staff_support.html'
+                            )
+                        );
+                        break;
+
+                    case self::PAGE_SEXUAL_ASSAULT:
+                        $out['content']['template'] = 'full_page_card.html';
+                        $out['content']['data'] = array(
+                            'title' => 'What is Sexual Assault?',
+                            'bottom_button' => array(
+                                'text' => 'Know more About Sexual Assault',
+                                'link' => self::getPageUrl(self::HOME, self::PAGE_SEXUAL_ASSAULT_MORE)
+                            ),
+                            'card_content' => array(
+                                'twig' => 'sexual_assault.html'
+                            )
+                        );
+                        break;
+
+                    case self::PAGE_SEXUAL_ASSAULT_MORE:
+                        $out['content']['template'] = 'full_page_card.html';
+                        $out['content']['data'] = array(
+                            'title' => 'What is Sexual Assault?',
+                            'card_content' => array(
+                                'twig' => 'sexual_assault_more.html'
                             )
                         );
                         break;
@@ -212,6 +243,61 @@ class Router
                         $out['javascripts'][] = 'multi_card.js';
                         break;
 
+                    case self::PAGE_COMMON_QUESTIONS:
+                        $out['content']['template'] = 'multi_segment_page.html';
+                        $out['content']['data'] = array(
+                            'title' => 'Sexual Assault Common Questions',
+                            'show_card_count' => 2,
+                            'cards_content' => array(
+                                0 => array(
+                                    'twig' => 'common_questions/ques_1.html'
+                                ),
+                                1 => array(
+                                    'twig' => 'common_questions/ques_2.html'
+                                ),
+                                2 => array(
+                                    'twig' => 'common_questions/ques_3.html'
+                                ),
+                                3 => array(
+                                    'twig' => 'common_questions/ques_4.html'
+                                ),
+                                4 => array(
+                                    'twig' => 'common_questions/ques_5.html'
+                                )
+                            )
+                        );
+                        $out['javascripts'][] = 'multi_segment.js';
+                        break;
+
+                    case self::PAGE_SAFETY_PLAN_WORKSHEET:
+                        $out['content']['template'] = 'multi_segment_page.html';
+                        $out['content']['data'] = array(
+                            'title' => 'Safety Plan Worksheet',
+                            'show_card_count' => 2,
+                            'cards_content' => array(
+                                0 => array(
+                                    'twig' => 'safety_plan_worksheet/concern_1.html'
+                                ),
+                                1 => array(
+                                    'twig' => 'safety_plan_worksheet/concern_2.html'
+                                ),
+                                2 => array(
+                                    'twig' => 'safety_plan_worksheet/concern_3.html'
+                                ),
+                                3 => array(
+                                    'twig' => 'safety_plan_worksheet/concern_4.html'
+                                ),
+                                4 => array(
+                                    'twig' => 'safety_plan_worksheet/concern_5.html'
+                                ),
+                                5 => array(
+                                    'twig' => 'safety_plan_worksheet/concern_6.html'
+                                )
+                            )
+                        );
+                        $out['javascripts'][] = 'multi_segment.js';
+                        break;
+
                     default:
                         break;
                 }
@@ -232,5 +318,19 @@ class Router
             $out['country_list'] = json_decode($countries, true);
         }
         return $out;
+    }
+
+    public static function getPageUrl($page, $content)
+    {
+        $pageUrl = array(
+            self::PAGE_REQUEST_PARAM => $page,
+            self::PAGE_CONTENT => $content
+        );
+        $url = HOST;
+        if (!empty($pageUrl)) {
+            $url .= '?';
+            $url .= http_build_query($pageUrl, '', '&');
+        }
+        return $url;
     }
 }
