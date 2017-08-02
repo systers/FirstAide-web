b10d8267e562abbb264adab6ca2d926d9f8abf92
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-08-02 17:02:14 +0530

    added positive and negative tests for notification validity

--- tests/modules/NotificationTest.php
+++ tests/modules/NotificationTest.php
@@ -16,6 +16,46 @@ use PHPUnit\Framework\TestCase;
 
 class NotificationTest extends TestCase
 {
+     /**
+     * Method : assertInvalidNotificationNotSent
+     * Description : Method to assert invalid notification messages
+     * @arrayOfMessages string : array of messages that need to be tested
+     */
+    private function assertInvalidNotificationNotSent(
+        array $arrayOfMessages
+    ) {
+        $counter = 0;
+        foreach ($arrayOfMessages as $m) {
+            $notification = new Notification(Notification::USE_TEST_CREDENTIALS);
+            $sendNotificationRespose = $notification->sendSms($m['phone'], $m['msg']);
+            $this->assertFalse($sendNotificationRespose['response']);
+            $counter += 1;
+        }
+
+        $this->assertEquals(count($arrayOfMessages), $counter);
+    }
+
+    /**
+     * Method : testInvalidNotificationParameters
+     * Description : Method to assert invalid notification parameters
+     */
+    public function testInvalidNotificationParameters()
+    {
+        $this->assertInvalidNotificationNotSent(
+            array(
+                array(
+                    'phone' => '',
+                    'msg' => 'This is a valid message'
+                ),
+                array(
+                    'phone' => '+910000000000',
+                    'msg' => ''
+                )
+            )
+        );
+    }
+
+
     /**
      * Method : testSendSms
      * Description : test to check of the response for sending a sms is a success
