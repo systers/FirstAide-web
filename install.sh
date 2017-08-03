b477c3cc86be1d5fbbb63af8fff57310675e3a21
Fatima Rafiqui <fatima.rafiqui@gmail.com>
2017-08-03 23:42:53 +0530

    Added dependencies to install shell script (#255)
    * Added dependencies to install shell script
    
    * Changing commenting style
    
    * Removing hash and minor fix

--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
+# install php7 and mysql
+sudo apt-get install php7.0 php7.0-fpm php7.0-mysql -y
+
+# install composer, a package dependency manager
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+# install node and npm for minification of css
+sudo apt-get install nodejs npm
+
+# install npm gulp cli globally for css minification
+npm install -g gulp-cli
+
+# installing packages using composer
 composer update
+
+# installing packages using npm (such as gulp)
 npm install
+
+# Changing permissions of cache directory
 sudo chmod -R 777 cache/
+
+# running gulp tasks for minifying css
 gulp
