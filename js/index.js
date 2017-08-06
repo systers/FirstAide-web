8571416caaa89f33d9628d137a01a8e8972056f2
FatimaRafiqui <fatima.rafiqui@gmail.com>
2017-08-06 19:13:51 +0530

    renamed javascripts directory

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-const validation = {
-    isEmailAddress:function(str) {
-        const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return pattern.test(str);
-    }
-}
 function showError(ele, msg) {
 	ele.addClass('error');
 	ele.find('.ui.red.pointing').text(msg);
