42ac61e302f472cb2bf5bad4455c812fb5bf62a3
FatimaRafiqui <fatima.rafiqui@gmail.com>
2017-08-14 23:37:19 +0530

    moved code to functions

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,31 @@
-$('.show-menu-button').on('click', function() {
+function showMenu() {
 	$('.ui.vertical.sidebar').addClass('overlay visible');
+}
+
+function hideMenu() {
+	$('.ui.vertical.sidebar.overlay.visible').removeClass('overlay visible');
+}
+
+function toggleSubMenuVisibility(ele) {
+	if (ele.is(':hidden')) {
+		ele.slideDown( "slow");
+	} else {
+		ele.slideUp( "slow");
+	}
+}
+
+$('.show-menu-button').on('click', function() {
+	showMenu();
 });
+
 $('body').on('click', function(e) {
     if ($(e.target).closest('.article').length > 0) {
-		$('.ui.vertical.sidebar.overlay.visible').removeClass('overlay visible');
+		hideMenu();
     }
 });
 
 $('.sub-menu-header').on('click', function() {
 	var thisParent = $(this).parent();
 	var thisMenu = thisParent.find('.menu');
-	if (thisMenu.is(':hidden')) {
-		thisMenu.slideDown( "slow");
-	} else {
-		thisMenu.slideUp( "slow");
-	}
+	toggleSubMenuVisibility(thisMenu);
 });
