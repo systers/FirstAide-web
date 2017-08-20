02921def05799f50a0595cd83ff76eaf1dbea433
FatimaRafiqui <fatima.rafiqui@gmail.com>
2017-08-20 23:25:39 +0530

    updated page title rendering

--- includes/page_top.php
+++ includes/page_top.php
@@ -19,14 +19,14 @@ if ($page['type'] == FirstAide\Router::INDEX) {
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 
         <!-- Site Properities -->
-        <title><?php echo isset($_page['title']) ? $_page['title'].' | ' : ''  ?>FirstAide, Peacecorps</title>
+        <title><?php echo (isset($page['title']) ? $page['title'].' | ' : '');  ?>FirstAide, Peacecorps</title>
         <?php
-        echo isset($_page['description']) ?
-            '<meta name="description" content="'.$_page['description'].'" />' :
+        echo isset($page['description']) ?
+            '<meta name="description" content="'.$page['description'].'" />' :
             '' ?>
         <?php
-        echo isset($_page['keywords']) ?
-        '<meta name="keywords" content="'.$_page['keywords'].'"
+        echo isset($page['keywords']) ?
+        '<meta name="keywords" content="'.$page['keywords'].'"
         />' : '' ?>
 
         <link rel="stylesheet" type="text/css" class="ui" href="stylesheets/min/semantic.min.css">
