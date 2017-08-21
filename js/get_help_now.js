c67af03ab9645946c774fa22fb8c186a3d197d83
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-08-21 18:27:41 +0530

    Refactoring js file for testability

--- js/get_help_now.js
+++ js/get_help_now.js
@@ -1,26 +1,42 @@
+/**
+ * showGetHelpActionSection() displays the page section
+ * where the user has options to seek help
+ * such as call or message
+ */
+function showGetHelpActionSection () {
+	var getHelpSection = $('#get-help-section'),
+		getHelpActionSection = $('#get-help-action-section');
+    getHelpSection.fadeOut('slow');
+    getHelpActionSection.delay(500).fadeIn('slow');
+}
+
+/**
+ * hideGetHelpActionSection() hides the get help page section
+ * and navigate to other section
+ */
+function hideGetHelpActionSection() {
+	var getHelpSection = $('#get-help-section'),
+		getHelpActionSection = $('#get-help-action-section');
+    getHelpActionSection.fadeOut('slow');
+    getHelpSection.delay(500).fadeIn('slow');
+    setTimeout(function() {
+		$('#content-container').addClass('hide');
+    	$('#content-container > div > div').addClass('hide');
+    }, 600);
+}
 $(document).ready(function() {
 	$('.contact-button').on('click', function() {
-		var thisId = $(this).attr('id'),
-			getHelpSection = $('#get-help-section'),
-			getHelpActionSection = $('#get-help-action-section');
+		var thisId = $(this).attr('id');
 		$('#get-help-call').attr('data-info', thisId);
 		$('#get-help-sms').attr('data-info', thisId);
 		if ($('#content-container #' + thisId + '-container').length > 0) {
 			$('#content-container').removeClass('hide');
 			$('#content-container #' + thisId + '-container').removeClass('hide');
 		}
-        getHelpSection.fadeOut('slow');
-        getHelpActionSection.delay(500).fadeIn('slow');
+		showGetHelpActionSection();
 	});
 
 	$('#get-help-action-section .remove.icon').on('click', function() {
-		var getHelpSection = $('#get-help-section'),
-		getHelpActionSection = $('#get-help-action-section');
-        getHelpActionSection.fadeOut('slow');
-        getHelpSection.delay(500).fadeIn('slow');
-        setTimeout(function() {
-			$('#content-container').addClass('hide');
-        	$('#content-container > div > div').addClass('hide');
-        }, 600);
+		hideGetHelpActionSection();
 	});
 });
